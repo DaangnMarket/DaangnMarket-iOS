f53e3a3015bc6a4c248e579555f955d2c073ede9
Mini <minnnidev@gmail.com>
2023-04-16 00:19:46 +0900

    [Setting] #1 - extension 추가

---
+++ DaangnMarket/DaangnMarket/Global/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  DaangnMarket
+//
+//  Created by 김민 on 2023/04/16.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+
+    static var cellIdentifier: String {
+        return String(describing: self)
+    }
+}

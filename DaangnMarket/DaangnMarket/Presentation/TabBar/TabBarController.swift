5c063f7c44a25329df106ef388eb9c78ce87e521
Mini <minnnidev@gmail.com>
2023-04-16 12:30:40 +0900

    [Design] #3 - 탭바 구현

--- DaangnMarket/DaangnMarket/Presentation/TabBar/TabBarController.swift
+++ DaangnMarket/DaangnMarket/Presentation/TabBar/TabBarController.swift
@@ -26,26 +26,48 @@ extension TabBarController {
     // MARK: - Custom Methods
     
     private func setTabBar() {
-//        let homeViewController = HomeViewController()
-//        let homeTab = UINavigationController(rootViewController: homeViewController)
-//        homeTab.isNavigationBarHidden = true
-//        homeTab.tabBarItem  = UITabBarItem(title: "홈", image: UIImage(named: "TabHome")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabHomeSelected")?.withRenderingMode(.alwaysOriginal))
-//        
-//        let townLifeViewController = TownLifeViewController()
-//        let townLifeTab = UINavigationController(rootViewController: townLifeViewController)
-//        townLifeTab.isNavigationBarHidden = true
-//        townLifeTab.tabBarItem  = UITabBarItem(title: "동네 생활", image: UIImage(named: "TabTownLife")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabTownLifeSelected")?.withRenderingMode(.alwaysOriginal))
-//        
-//        let NearMeViewController = NearMeViewController()
-//        let nearMeTab = UINavigationController(rootViewController: NearMeViewController)
-//        nearMeTab.isNavigationBarHidden = true
-//        nearMeTab.tabBarItem  = UITabBarItem(title: "내 근처", image: UIImage(named: "TabNearMe")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabNearMeSelected")?.withRenderingMode(.alwaysOriginal))
-//        
-//        let tabs = [homeTab, townLifeTab, nearMeTab]
-//
-//        self.setViewControllers(tabs, animated: false)
-//        
-//        tabBar.backgroundColor = .white
-//        tabBar.tintColor = .black
+        let homeViewController = HomeViewController()
+        let homeTab = UINavigationController(rootViewController: homeViewController)
+        homeTab.isNavigationBarHidden = true
+        homeTab.tabBarItem  = UITabBarItem(title: "홈", image: UIImage(named: "TabHome")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabHomeSelected")?.withRenderingMode(.alwaysOriginal))
+
+        let townLifeViewController = TownLifeViewController()
+        let townLifeTab = UINavigationController(rootViewController: townLifeViewController)
+        townLifeTab.isNavigationBarHidden = true
+        townLifeTab.tabBarItem  = UITabBarItem(title: "동네 생활", image: UIImage(named: "TabTownLife")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabTownLifeSelected")?.withRenderingMode(.alwaysOriginal))
+
+        let nearMeViewController = NearMeViewController()
+        let nearMeTab = UINavigationController(rootViewController: nearMeViewController)
+        nearMeTab.isNavigationBarHidden = true
+        nearMeTab.tabBarItem  = UITabBarItem(title: "내 근처", image: UIImage(named: "TabNearMe")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabNearMeSelected")?.withRenderingMode(.alwaysOriginal))
+        
+        let chatViewController = ChatViewController()
+        let chattingTab = UINavigationController(rootViewController: chatViewController)
+        chattingTab.isNavigationBarHidden = true
+        chattingTab.tabBarItem  = UITabBarItem(title: "채팅", image: UIImage(named: "TabChatting")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabChattingSelected")?.withRenderingMode(.alwaysOriginal))
+        
+        let myDaangnViewController = MyDaangnViewController()
+        let myDaangnTab = UINavigationController(rootViewController: myDaangnViewController)
+        myDaangnTab.isNavigationBarHidden = true
+        myDaangnTab.tabBarItem  = UITabBarItem(title: "내", image: UIImage(named: "TabMyDaangn")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabMyDaangnSelected")?.withRenderingMode(.alwaysOriginal))
+
+        let tabs = [homeTab, townLifeTab, nearMeTab, chattingTab, myDaangnTab]
+
+        self.setViewControllers(tabs, animated: false)
+        
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
+        tabBar.layer.borderWidth = 0.5
+        tabBar.layer.borderColor = UIColor.lightGray.cgColor
+        tabBar.clipsToBounds = true
+    }
+}
+
+extension UITabBar {
+    
+    static func clearShadow() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor.white
     }
 }

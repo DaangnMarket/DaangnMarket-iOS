1fe3f167b9d1f502fb71b49e6fd512926168a182
Mini <minnnidev@gmail.com>
2023-04-16 15:08:44 +0900

    [Fix] #3 - 탭바 이미지 오류 수정

--- DaangnMarket/DaangnMarket/Presentation/TabBar/TabBarController.swift
+++ DaangnMarket/DaangnMarket/Presentation/TabBar/TabBarController.swift
@@ -25,31 +25,43 @@ extension TabBarController {
     
     // MARK: - Custom Methods
     
+    private func makeTabBar(viewController: UIViewController,
+                            title: String,
+                            tabBarImage: UIImage,
+                            tabBarImageSelected : UIImage) -> UIViewController {
+        let tab = UINavigationController(rootViewController: viewController)
+        tab.isNavigationBarHidden = true
+        tab.tabBarItem = UITabBarItem(title: title,
+                                      image: tabBarImage.withRenderingMode(.alwaysOriginal),
+                                      selectedImage: tabBarImageSelected.withRenderingMode(.alwaysOriginal))
+        return tab
+    }
+    
     private func setTabBar() {
-        let homeViewController = HomeViewController()
-        let homeTab = UINavigationController(rootViewController: homeViewController)
-        homeTab.isNavigationBarHidden = true
-        homeTab.tabBarItem  = UITabBarItem(title: "홈", image: UIImage(named: "TabHome")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabHomeSelected")?.withRenderingMode(.alwaysOriginal))
-
-        let townLifeViewController = TownLifeViewController()
-        let townLifeTab = UINavigationController(rootViewController: townLifeViewController)
-        townLifeTab.isNavigationBarHidden = true
-        townLifeTab.tabBarItem  = UITabBarItem(title: "동네 생활", image: UIImage(named: "TabTownLife")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabTownLifeSelected")?.withRenderingMode(.alwaysOriginal))
-
-        let nearMeViewController = NearMeViewController()
-        let nearMeTab = UINavigationController(rootViewController: nearMeViewController)
-        nearMeTab.isNavigationBarHidden = true
-        nearMeTab.tabBarItem  = UITabBarItem(title: "내 근처", image: UIImage(named: "TabNearMe")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabNearMeSelected")?.withRenderingMode(.alwaysOriginal))
+        let homeTab = makeTabBar(viewController: HomeViewController(),
+                                 title: "홈",
+                                 tabBarImage: UIImage(named: "TabHome")!,
+                                 tabBarImageSelected: UIImage(named: "TabHomeSelected")!)
         
-        let chatViewController = ChatViewController()
-        let chattingTab = UINavigationController(rootViewController: chatViewController)
-        chattingTab.isNavigationBarHidden = true
-        chattingTab.tabBarItem  = UITabBarItem(title: "채팅", image: UIImage(named: "TabChatting")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabChattingSelected")?.withRenderingMode(.alwaysOriginal))
+        let townLifeTab = makeTabBar(viewController: TownLifeViewController(),
+                                 title: "동네 생활",
+                                 tabBarImage: UIImage(named: "TabTownLife")!,
+                                 tabBarImageSelected: UIImage(named: "TabTownLifeSelected")!)
         
-        let myDaangnViewController = MyDaangnViewController()
-        let myDaangnTab = UINavigationController(rootViewController: myDaangnViewController)
-        myDaangnTab.isNavigationBarHidden = true
-        myDaangnTab.tabBarItem  = UITabBarItem(title: "내", image: UIImage(named: "TabMyDaangn")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "TabMyDaangnSelected")?.withRenderingMode(.alwaysOriginal))
+        let nearMeTab = makeTabBar(viewController: NearMeViewController(),
+                                 title: "내 근처",
+                                 tabBarImage: UIImage(named: "TabNearMe")!,
+                                 tabBarImageSelected: UIImage(named: "TabNearMeSelected")!)
+        
+        let chattingTab = makeTabBar(viewController: ChatViewController(),
+                                 title: "채팅",
+                                 tabBarImage: UIImage(named: "TabChatting")!,
+                                 tabBarImageSelected: UIImage(named: "TabChattingSelected")!)
+        
+        let myDaangnTab = makeTabBar(viewController: MyDaangnViewController(),
+                                 title: "나의 당근",
+                                 tabBarImage: UIImage(named: "TabMyDaangn")!,
+                                 tabBarImageSelected: UIImage(named: "TabMyDaangnSelected")!)
 
         let tabs = [homeTab, townLifeTab, nearMeTab, chattingTab, myDaangnTab]
 

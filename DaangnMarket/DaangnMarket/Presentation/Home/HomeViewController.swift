eb8216475a6f5b0670251d58c9a3505e6a54fb48
Taerogrammer <hyung54321@naver.com>
2023-04-17 16:49:10 +0900

    [Feat] #7 - 홈 네비게이션 바 생성

--- DaangnMarket/DaangnMarket/Presentation/Home/HomeViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/Home/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         setBackgroundColor()
+        homeNavigationBar()
     }
 }
 
@@ -24,5 +25,48 @@ extension HomeViewController {
     
     private func setBackgroundColor() {
         view.backgroundColor = .white
+    }
+    
+    private func homeNavigationBar() {
+        let noticeImage = UIImage(systemName: "bell")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        let noticeButton = UIButton()
+        noticeButton.setImage(noticeImage, for: .normal)
+        noticeButton.addTarget(self, action: #selector(goToNoticeVC), for: .touchUpInside)
+        let notice = UIBarButtonItem(customView: noticeButton)
+                
+        let categoryImage = UIImage(systemName: "line.3.horizontal")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        let categoryButton = UIButton()
+        categoryButton.setImage(categoryImage, for: .normal)
+        categoryButton.addTarget(self, action: #selector(goToSearchVC), for: .touchUpInside)
+        let category = UIBarButtonItem(customView: categoryButton)
+    
+        let searchImage = UIImage(systemName: "magnifyingglass")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        let searchButton = UIButton()
+        searchButton.setImage(searchImage, for: .normal)
+        searchButton.addTarget(self, action: #selector(goToSearchVC), for: .touchUpInside)
+        let search = UIBarButtonItem(customView: searchButton)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 16
+        
+        navigationItem.rightBarButtonItems = [notice, spacer, category, spacer, search]
+    }
+    
+    @objc fileprivate func goToSearchVC() {
+        print("goToSearchVC() called")
+        let searchVC = SearchViewController()
+        self.navigationController?.pushViewController(searchVC, animated: true)
+    }
+    
+    @objc fileprivate func goToCategoryVC() {
+        print("goToCategoryVC() called")
+//        let searchVC = SearchViewController()
+//        self.navigationController?.pushViewController(searchVC, animated: true)
+    }
+    
+    @objc fileprivate func goToNoticeVC() {
+        print("goToSearchVC() called")
+        let searchVC = NoticeViewController()
+        self.navigationController?.pushViewController(searchVC, animated: true)
     }
 }

c3d76c287340c9cacc1a79ecbdd86c299ebfccdc
Taerogrammer <hyung54321@naver.com>
2023-04-18 14:59:10 +0900

    [Feat]#7 : MARK 주석, 함수명 변경

--- DaangnMarket/DaangnMarket/Presentation/Home/HomeViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/Home/HomeViewController.swift
@@ -31,19 +31,19 @@ extension HomeViewController {
         let noticeImage = UIImage(systemName: "bell")?.withTintColor(.black, renderingMode: .alwaysOriginal)
         let noticeButton = UIButton()
         noticeButton.setImage(noticeImage, for: .normal)
-        noticeButton.addTarget(self, action: #selector(goToNoticeVC), for: .touchUpInside)
+        noticeButton.addTarget(self, action: #selector(noticeButtonDidTap), for: .touchUpInside)
         let notice = UIBarButtonItem(customView: noticeButton)
                 
         let categoryImage = UIImage(systemName: "line.3.horizontal")?.withTintColor(.black, renderingMode: .alwaysOriginal)
         let categoryButton = UIButton()
         categoryButton.setImage(categoryImage, for: .normal)
-        categoryButton.addTarget(self, action: #selector(goToSearchVC), for: .touchUpInside)
+        categoryButton.addTarget(self, action: #selector(categoryButtonDidTap), for: .touchUpInside)
         let category = UIBarButtonItem(customView: categoryButton)
     
         let searchImage = UIImage(systemName: "magnifyingglass")?.withTintColor(.black, renderingMode: .alwaysOriginal)
         let searchButton = UIButton()
         searchButton.setImage(searchImage, for: .normal)
-        searchButton.addTarget(self, action: #selector(goToSearchVC), for: .touchUpInside)
+        searchButton.addTarget(self, action: #selector(searchButtonDidTap), for: .touchUpInside)
         let search = UIBarButtonItem(customView: searchButton)
         
         let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
@@ -52,19 +52,21 @@ extension HomeViewController {
         navigationItem.rightBarButtonItems = [notice, spacer, category, spacer, search]
     }
     
-    @objc fileprivate func goToSearchVC() {
+    //MARK: - @objc Methods
+    
+    @objc fileprivate func searchButtonDidTap() {
         print("goToSearchVC() called")
         let searchVC = SearchViewController()
         self.navigationController?.pushViewController(searchVC, animated: true)
     }
     
-    @objc fileprivate func goToCategoryVC() {
+    @objc fileprivate func categoryButtonDidTap() {
         print("goToCategoryVC() called")
 //        let searchVC = SearchViewController()
 //        self.navigationController?.pushViewController(searchVC, animated: true)
     }
     
-    @objc fileprivate func goToNoticeVC() {
+    @objc fileprivate func noticeButtonDidTap() {
         print("goToSearchVC() called")
         let searchVC = NoticeViewController()
         self.navigationController?.pushViewController(searchVC, animated: true)

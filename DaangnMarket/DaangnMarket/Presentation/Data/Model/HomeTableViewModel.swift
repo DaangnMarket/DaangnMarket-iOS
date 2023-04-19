a2c8a1c71b998681f17952714ad8ca204e2191d1
Taerogrammer <hyung54321@naver.com>
2023-04-20 00:27:03 +0900

    feature/#14

---
+++ DaangnMarket/DaangnMarket/Presentation/Data/Model/HomeTableViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTableViewData.swift
+//  DaangnMarket
+//
+//  Created by KimTaeHyung on 2023/04/20.
+//
+
+import UIKit
+
+struct HomeTableViewModel {
+    var image: UIImage
+    var title: String
+    var location: String
+    var time: String
+    var price: Int
+    var likeCount: Int
+    var chattingCount: Int
+}

44a35c22033cdac0399ecb8120a0dffa5e708bb9
Taerogrammer <hyung54321@naver.com>
2023-04-25 16:53:05 +0900

    [Feat] DaangnMarket#31 - ProductView 네비게이션바 생성

--- DaangnMarket/DaangnMarket/Presentation/Product/ViewController/ProductViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/Product/ViewController/ProductViewController.swift
@@ -17,7 +17,8 @@ class ProductViewController: UIViewController {
         super.viewDidLoad()
         
         setBackgroundColor()
-        print(homeData)
+        navigationLeftBar()
+        navigationRightBar()
     }
     
 
@@ -28,7 +29,67 @@ extension ProductViewController {
     // MARK: - Custom Methods
     
     private func setBackgroundColor() {
-        view.backgroundColor = .white
+        view.backgroundColor = .black
+    }
+    
+    private func navigationLeftBar() {
+        let backButtonImage = UIImage(systemName: "chevron.left")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        let backButton = UIButton()
+        backButton.setImage(backButtonImage, for: .normal)
+        backButton.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+        let back = UIBarButtonItem(customView: backButton)
+        
+        let homeButtonImage = UIImage(named: "TabHome")
+        let homeButton = UIButton()
+        homeButton.setImage(homeButtonImage, for: .normal)
+        homeButton.addTarget(self, action: #selector(homeButtonDidTap), for: .touchUpInside)
+        let home = UIBarButtonItem(customView: homeButton)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 16
+        
+        navigationItem.leftBarButtonItems = [back, spacer, home]
+        
+    }
+    
+    private func navigationRightBar() {
+        let shareButtonImage = UIImage(systemName: "square.and.arrow.up")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        let shareButton = UIButton()
+        shareButton.setImage(shareButtonImage, for: .normal)
+        shareButton.addTarget(self, action: #selector(shareButtonDidTap), for: .touchUpInside)
+        let share = UIBarButtonItem(customView: shareButton)
+        
+        let declareButtonImage = UIImage(systemName: "ellipsis")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        let declareButton = UIButton()
+        declareButton.setImage(declareButtonImage, for: .normal)
+        declareButton.addTarget(self, action: #selector(declareButtonDidTap), for: .touchUpInside)
+        let declare = UIBarButtonItem(customView: declareButton)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 16
+        
+        navigationItem.rightBarButtonItems = [declare, spacer, share]
+        
+    }
+    
+    
+    //MARK: - @objc Methods
+    
+    @objc fileprivate func backButtonDidTap() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    @objc fileprivate func homeButtonDidTap() {
+        let homeVC = HomeViewController()
+        self.navigationController?.pushViewController(homeVC, animated: true)
+    }
+    
+    @objc fileprivate func shareButtonDidTap() {
+        print("shareButtonDidTap() called")
+    }
+    
+    @objc fileprivate func declareButtonDidTap() {
+        print("shareButtonDidTap() called")
     }
 
 }

3f09a9165c8a43fd348d6324bd2ee3e5121c7b2e
Mini <minnnidev@gmail.com>
2023-04-28 20:03:28 +0900

    [Design] #20 - 배경색 수정

--- DaangnMarket/DaangnMarket/Presentation/MyDaangn/ViewController/ProfileViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/MyDaangn/ViewController/ProfileViewController.swift
@@ -29,7 +29,7 @@ extension ProfileViewController {
     // MARK: - Custom Methods
     
     private func setBackgroundColor() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white 
     }
     
     private func setLayout() {

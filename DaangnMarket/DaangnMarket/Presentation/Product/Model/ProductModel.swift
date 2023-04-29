40638a5b25fd6961ca7804c59bdc860fcaf26262
Taerogrammer <hyung54321@naver.com>
2023-04-29 11:47:58 +0900

    [Design]/DaangnMarket#33 - 프로덕트뷰 레이아웃 구현 - Model 생성

---
+++ DaangnMarket/DaangnMarket/Presentation/Product/Model/ProductModel.swift
@@ -0,0 +1,27 @@
+//
+//  ProductModel.swift
+//  DaangnMarket
+//
+//  Created by KimTaeHyung on 2023/04/29.
+//
+
+import UIKit
+
+struct ProductModel {
+    
+    var id: Int
+    var images: [UIImage]?
+    var profileImage: UIImage
+    var nickname: String
+    var address: String
+    var mannerTemp: String
+    var title: String
+    var category: String
+    var createdAt: String
+    var contents: String
+    var numberOfChat: Int
+    var numberOfInterest: Int
+    var numberOfView: Int
+    var price: Int
+    
+}

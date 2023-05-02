592a2d5dddfdad83bb6789db7f4dde3cfa49fa2f
Mini <minnnidev@gmail.com>
2023-05-02 10:27:01 +0900

    [Feat] #41 - 프로필 -> 닉네임수정 화면 전환 추가

--- DaangnMarket/DaangnMarket/Presentation/Profile/View/ProfileHeaderView.swift
+++ DaangnMarket/DaangnMarket/Presentation/Profile/View/ProfileHeaderView.swift
@@ -10,7 +10,9 @@ import UIKit
 import SnapKit
 import Then
 
-protocol 
+protocol ProfileActionProtocol {
+    func modifyNicknameButtonDidTap()
+}
 
 final class ProfileHeaderView: UIView {
     
@@ -136,6 +138,10 @@ final class ProfileHeaderView: UIView {
         $0.font = .systemFont(ofSize: 13)
         $0.textColor = .lightGray
     }
+    
+    // MARK: - Properties
+    
+    var delegate: ProfileActionProtocol?
 
     // MARK: - Initializer
     
@@ -144,6 +150,7 @@ final class ProfileHeaderView: UIView {
         
         setBackgroundColor()
         setLayout()
+        setAddTarget()
     }
     
     required init?(coder: NSCoder) {
@@ -271,5 +278,15 @@ extension ProfileHeaderView {
         let progressViewWidth = getScreenWidth() - 30
         let firstProgress = progressViewWidth * 0.365
         return firstProgress
+    }
+    
+    private func setAddTarget() {
+        modifyProfileButton.addTarget(self, action: #selector(modifyNicknameButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func modifyNicknameButtonDidTap() {
+        delegate?.modifyNicknameButtonDidTap()
     }
 }
---
+++ DaangnMarket/DaangnMarket/Presentation/Profile/ViewController/ModifyNicknameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyNicknameViewController.swift
+//  DaangnMarket
+//
+//  Created by 김민 on 2023/05/02.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ModifyNicknameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+}

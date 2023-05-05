3cdcbe5aafe86e7b075980f695d9b3a5e33a6fb1
Taerogrammer <hyung54321@naver.com>
2023-05-05 10:37:56 +0900

    [Add] DaangnMarket#33 - 다른 판매상품 모델 추가

---
+++ DaangnMarket/DaangnMarket/Presentation/Product/Model/OtherProductModel.swift
@@ -0,0 +1,17 @@
+//
+//  SellingModel.swift
+//  DaangnMarket
+//
+//  Created by KimTaeHyung on 2023/05/05.
+//
+
+import UIKit
+
+struct OtherProductModel {
+    
+    var id: Int
+    var image: String
+    var title: String
+    var price: Int
+    
+}

124315d8af63c5ce6e3ee79042d7325a0d131b7b
Taerogrammer <hyung54321@naver.com>
2023-05-22 17:29:21 +0900

    [Feat]DaaangnMarket#37 - 내 동네 하나일 때 설정 완료

---
+++ DaangnMarket/DaangnMarket/Presentation/SearchAddress/ViewController/SearchAddressViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchAddressViewController.swift
+//  DaangnMarket
+//
+//  Created by KimTaeHyung on 2023/05/22.
+//
+
+import UIKit
+
+class SearchAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+}

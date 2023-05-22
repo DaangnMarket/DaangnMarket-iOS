40759a7b753136e2e3046735b95ee0ef19d97fde
Mini <minnnidev@gmail.com>
2023-05-22 22:47:12 +0900

    [Design] #61 - 관심목록뷰 구현

--- DaangnMarket/DaangnMarket/Presentation/MyDaangn/ViewController/WishListViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/MyDaangn/ViewController/WishListViewController.swift
@@ -11,13 +11,28 @@ import SnapKit
 import Then
 
 final class WishListViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private lazy var wishTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .clear
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
+    }()
 
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       setNavigationBar()
+        setBackgroundColor()
+        setNavigationBar()
+        setLayout()
+        register()
+        setDelegate()
     }
 }
 
@@ -25,7 +40,54 @@ extension WishListViewController {
     
     // MARK: - Custom Methods
     
+    private func setBackgroundColor() {
+        view.backgroundColor = .white
+    }
+    
     private func setNavigationBar() {
-        view.backgroundColor = .yellow
+        setNavigationBackground()
+        
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonDidTap))
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.title = "관심목록"
+        navigationItem.leftBarButtonItem?.tintColor = .black
+    }
+    
+    private func setLayout() {
+        view.addSubview(wishTableView)
+        
+        wishTableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func register() {
+        wishTableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.cellIdentifier)
+    }
+    
+    private func setDelegate() {
+        wishTableView.dataSource = self
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func backButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension WishListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.cellIdentifier, for: indexPath) as! HomeTableViewCell
+        return cell
     }
 }

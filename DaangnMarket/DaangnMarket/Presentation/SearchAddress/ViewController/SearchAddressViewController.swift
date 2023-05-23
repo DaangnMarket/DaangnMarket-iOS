9c8e944697c180da6b49f4c2203b5d5b190fa2db
Taerogrammer <hyung54321@naver.com>
2023-05-23 10:41:45 +0900

    [Feat]DaangnMarket#60 - 현재 위치 표시하기 버튼 생성

--- DaangnMarket/DaangnMarket/Presentation/SearchAddress/ViewController/SearchAddressViewController.swift
+++ DaangnMarket/DaangnMarket/Presentation/SearchAddress/ViewController/SearchAddressViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class SearchAddressViewController: UIViewController {
+    
+    //MARK: - UI Components
+    
+    let searchBar = UISearchBar()
+    let emptyImage = UIImage()
 
+    let currentLocationButton = UIButton()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,9 +38,7 @@ extension SearchAddressViewController {
 
     private func setNavigationBar() {
         // Search Bar
-        let searchBar = UISearchBar()
         searchBar.placeholder = "􀊫 동명(읍,면)으로 검색 (ex. 서초동)"
-        let emptyImage = UIImage()
         searchBar.setImage(emptyImage, for: .search, state: .normal)
         
         let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"),
@@ -41,8 +47,33 @@ extension SearchAddressViewController {
                                          action: #selector(UINavigationController.popViewController(animated:)))
         backButton.tintColor = .black
         navigationItem.leftBarButtonItem = backButton
-        // navigationController?.interactivePopGestureRecognizer?.delegate = self
         navigationItem.titleView = searchBar
         navigationItem.hidesSearchBarWhenScrolling = false
+        
+        setCurrentLocationButton()
+    }
+    
+    private func setCurrentLocationButton() {
+        currentLocationButton.setTitle("􀐩 현재위치로 찾기", for: .normal)
+        currentLocationButton.setTitleColor(.white, for: .normal)
+        currentLocationButton.backgroundColor = .orange
+        currentLocationButton.layer.cornerRadius = 6
+        currentLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        currentLocationButton.addTarget(self, action: #selector(getCurrentLocation), for: .touchUpInside)
+        view.addSubview(currentLocationButton)
+        
+        NSLayoutConstraint.activate([
+            currentLocationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            currentLocationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            currentLocationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8), // Position below the safe area top
+            currentLocationButton.heightAnchor.constraint(equalToConstant: 40) // Set a specific height for the button
+        ])
+        
+    }
+    
+    //MARK: - @objc Methods
+    
+    @objc fileprivate func getCurrentLocation() {
+        print("getCurrentLocation() clicked")
     }
 }
